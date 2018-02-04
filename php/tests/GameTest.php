19ecb9fb43a4f9445a7759ccd6f40eb709492b24
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 16:28:33 +0100

    PHP: Capture game run output in a file to be able to compare two different runs

--- php/tests/GameTest.php
+++ php/tests/GameTest.php
@@ -5,15 +5,18 @@ use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
 {
-    public function testItWorks()
-    {
-        $this->assertEquals(true, true);
-    }
-
     public function testGameRun()
     {
+        $ob_file = fopen('php-trivia-output-2.txt', 'w');
+        $ob_file_callback = function($buffer) use ($ob_file) {
+            fwrite($ob_file, $buffer);
+        };
+        ob_start($ob_file_callback);
+
         srand(11);
         run();
+
+        ob_end_flush();
     }
 }
 ?>
