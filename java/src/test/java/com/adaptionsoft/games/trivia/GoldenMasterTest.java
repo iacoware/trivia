4c027381249df450c5d55a97c11bb713a611f623
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 13:50:32 +0100

    Java: update golden master

--- java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -3,10 +3,10 @@ package com.adaptionsoft.games.trivia;
 import static org.junit.Assert.*;
 
 import com.adaptionsoft.games.trivia.runner.GameRunner;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
@@ -20,19 +20,29 @@ public class GoldenMasterTest {
 	public static final String RUN_OUTPUT = "trivia-current-output.txt";
 
 	@Test
+	@Ignore
+	public void updateGoldenMaster() throws IOException {
+		runMultipleTimesUsing(GOLDEN_MASTER);
+	}
+
+	@Test
 	public void runGame() throws IOException {
+		runMultipleTimesUsing(RUN_OUTPUT);
+
+		assertEquals(contentOf(GOLDEN_MASTER), contentOf(RUN_OUTPUT));
+	}
+
+	private void runMultipleTimesUsing(String outputFile) throws FileNotFoundException {
 		PrintStream prevOut = System.out;
-		System.setOut(new PrintStream(RUN_OUTPUT));
+		System.setOut(new PrintStream(outputFile));
 
 		try {
-			for (int i = 0; i < 1; i++) {
-				GameRunner.run(new Random(11));
+			for (int i = 0; i < 1000; i++) {
+				GameRunner.run(new Random(11 * i));
 			}
 		} finally {
 			System.setOut(prevOut);
 		}
-
-		assertEquals(contentOf(GOLDEN_MASTER), contentOf(RUN_OUTPUT));
 	}
 
 	private List<String> contentOf(String s) throws IOException {
