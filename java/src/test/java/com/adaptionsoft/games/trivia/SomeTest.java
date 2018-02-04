745f13dad2179f73b6d3ee1f0876cadd96b9b7da
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 12:54:24 +0100

    Java: capture output to file (first step to golden master). Ouput is not deterministic

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -5,10 +5,14 @@ import static org.junit.Assert.*;
 import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class SomeTest {
 
 	@Test
-	public void runGame() {
+	public void runGame() throws FileNotFoundException {
+		System.setOut(new PrintStream("trivia-output-2.txt"));
 		GameRunner.main(null);
 	}
 }
