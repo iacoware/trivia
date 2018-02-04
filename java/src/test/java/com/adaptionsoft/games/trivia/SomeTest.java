7d9bbe033af115abed7adabb4ffef5ba64c98fc8
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 12:48:41 +0100

    Java: run game from test

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -2,12 +2,13 @@ package com.adaptionsoft.games.trivia;
 
 import static org.junit.Assert.*;
 
+import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.Test;
 
 public class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
-		assertTrue(false);
+	public void runGame() {
+		GameRunner.main(null);
 	}
 }
