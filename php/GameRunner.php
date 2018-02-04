b48c20d85b6099915717b354cad9246a89735be7
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 15:51:05 +0100

    PHP: moved GameRunner body GameShell.run function to separate include from run

--- php/GameRunner.php
+++ php/GameRunner.php
@@ -1,27 +1,7 @@
 <?php
 
-include __DIR__.'/Game.php';
+include __DIR__.'/GameShell.php';
 
-$notAWinner;
+run();
 
-  $aGame = new Game();
-  
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
-  
-  
-  do {
-    
-    $aGame->roll(rand(0,5) + 1);
-    
-    if (rand(0,9) == 7) {
-      $notAWinner = $aGame->wrongAnswer();
-    } else {
-      $notAWinner = $aGame->wasCorrectlyAnswered();
-    }
-    
-    
-    
-  } while ($notAWinner);
-  
+?>
--- php/tests/GameTest.php
+++ php/tests/GameTest.php
@@ -1,4 +1,6 @@
 <?php
+
+include __DIR__.'/../GameShell.php';
 use PHPUnit\Framework\TestCase;
 
 class StackTest extends TestCase
@@ -6,6 +8,11 @@ class StackTest extends TestCase
     public function testItWorks()
     {
         $this->assertEquals(true, true);
+    }
+
+    public function testGameRun()
+    {
+        run();
     }
 }
 ?>
