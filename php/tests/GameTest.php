5c7762356fbeb5aca6934b7e0f429f9702533ff3
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 17:46:08 +0100

    PHP: added function to ease golden master update

--- php/tests/GameTest.php
+++ php/tests/GameTest.php
@@ -8,25 +8,31 @@ class GoldenMasterTest extends TestCase
     const GOLDEN_MASTER = 'trivia-golden-master.txt';
     const CURRENT_OUTPUT = 'trivia-current-output.txt';
 
-    public function testGameRun()
-    {
-        //$ob_file = fopen(self::GOLDEN_MASTER, 'w');
-        $ob_file = fopen(self::CURRENT_OUTPUT, 'w');
+    public function _testUpdateGoldenMaster() {
+        $this->runGameMultipleTimes(self::GOLDEN_MASTER);
+    }
+
+    public function testGameRun() {
+        $this->runGameMultipleTimes(self::CURRENT_OUTPUT);
+
+        $golden_master = file_get_contents(self::GOLDEN_MASTER);
+        $current_output = file_get_contents(self::CURRENT_OUTPUT);
+        $this->assertEquals($golden_master, $current_output);
+    }
+
+    public function runGameMultipleTimes($outputFile) {
+        $ob_file = fopen($outputFile, 'w');
         $ob_file_callback = function($buffer) use ($ob_file) {
             fwrite($ob_file, $buffer);
         };
         ob_start($ob_file_callback);
 
-        for($i = 0; $i < 10; $i++) {
+        for($i = 0; $i < 20; $i++) {
             srand(11 * $i);
             run();
         }
 
         ob_end_flush();
-
-        $golden_master = file_get_contents(self::GOLDEN_MASTER);
-        $current_output = file_get_contents(self::CURRENT_OUTPUT);
-        $this->assertEquals($golden_master, $current_output);
     }
 }
 ?>
