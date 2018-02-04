ae558b5a9bbef727376fc3be21c39906873494de
Massimo Iacolare <info@massimoiacolare.it>
2018-02-04 13:06:01 +0100

    Java: golden master test

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -6,13 +6,22 @@ import com.adaptionsoft.games.trivia.runner.GameRunner;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Random;
 
 public class SomeTest {
 	@Test
-	public void runGame() throws FileNotFoundException {
+	public void runGame() throws IOException {
 		System.setOut(new PrintStream("trivia-current-output.txt"));
 		GameRunner.run(new Random(11));
+
+		List<String> goldenMaster = Files.readAllLines(Paths.get("trivia-golden-master.txt"));
+		List<String> currentOutput = Files.readAllLines(Paths.get("trivia-current-output.txt"));
+
+		assertEquals(goldenMaster, currentOutput);
 	}
 }
