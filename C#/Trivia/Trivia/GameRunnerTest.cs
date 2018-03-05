dda58557fa1177a4484e5b120f34ac8752eb708a
Massimo Iacolare <info@massimoiacolare.it>
2018-03-05 16:53:42 +0100

    C#: run Game from test

---
+++ C#/Trivia/Trivia/GameRunnerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Trivia
+{
+    public class GameRunnerTest
+    {
+        [Fact]
+        public void RunGame()
+        {
+            GameRunner.Run(new Random(11));
+        }
+    }
+}
